5df1c90438f9ca94fdd0f323d48c010330fa5faf
Abrar Mahmud Hasan <abrarhasan2003@gmail.com>
2026-03-01 23:28:24 +0600

    Minor UI update

--- Frontend/config/firebase.js
+++ Frontend/config/firebase.js
@@ -3,5 +3,5 @@ export const CONFIG = {
     // or 'http://10.0.2.2:3000' for Android Emulator
     // or 'http://localhost:3000' for Web/iOS Simulator.
     // Stable Local IP Address
-    BACKEND_URL: 'http://192.168.0.105:3000/api'
+    BACKEND_URL: 'http://10.11.48.236:3000/api'
 };
